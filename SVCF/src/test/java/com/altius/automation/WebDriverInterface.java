8fa9a94333dd1abeb49d845fc6e7ae0aca8bedf6
Vignesh <dpvignesh1997@gmail.com>
2019-04-15 11:19:59 +0530

    changes on 15/04/2019

---
+++ SVCF/src/test/java/com/altius/automation/WebDriverInterface.java
@@ -0,0 +1,8 @@
+package com.altius.automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public interface WebDriverInterface {
+	//anae = "This is s a interface";
+}
